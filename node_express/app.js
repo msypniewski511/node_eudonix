a7dd9f2a375e5d57097f083beb16953681e85d98
Maciej Sypniewski <msypniewski511@gmail.com>
2018-08-31 00:48:00 +0100

    Bootstrap page routes & views

--- node_express/app.js
+++ node_express/app.js
@@ -5,12 +5,26 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
+// Set jade as view template
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
   console.log("Hello World");
-  res.send("<h1>Hello World</h1>");
+  res.render("index", { active: "index" });
+});
+
+app.get("/about", function(req, res) {
+  console.log("Hello World");
+  res.render("about", { title: "Welcom", active: "about" });
+});
+
+app.get("/contact", function(req, res) {
+  console.log("Hello World");
+  res.render("contact", { title: "Welcom", active: "contact" });
 });
 
 app.listen(3000);
