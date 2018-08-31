00a5210d317840394caf151f9e88fb066ce8bc6a
Maciej Sypniewski <msypniewski511@gmail.com>
2018-08-31 16:42:47 +0100

    Completed Simple web app with express, ade and nodemailer

--- node_express/app.js
+++ node_express/app.js
@@ -27,5 +27,51 @@ app.get("/contact", function(req, res) {
   res.render("contact", { title: "Welcom", active: "contact" });
 });
 
+app.post("/contact/send", function(req, res) {
+  console.log("test");
+  console.log(req.body.email);
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "msypniewski511@gmail.com",
+      pass: "XXXXXXXX"
+    }
+  });
+
+  let mailOption = {
+    from: "Maciej Sypniewski <msypniewski511@gmail>",
+    to: `${req.body.email}`,
+    subject: "Website Submission",
+    text:
+      "You have a submission with the following details...Name: " +
+      req.body.name +
+      " Email: " +
+      req.body.email +
+      "Message: " +
+      req.body.message,
+    html:
+      "<p>You have a submission with the following details...<ul>" +
+      "<li>Name: " +
+      req.body.name +
+      "</li>" +
+      "<li>Email: " +
+      req.body.email +
+      "</li>" +
+      "<li>Message: " +
+      req.body.message +
+      "</li>" +
+      "</ul></p>"
+  };
+
+  transporter.sendMail(mailOption, function(error, info) {
+    if (error) {
+      console.log(error);
+      res.redirect("/");
+    } else {
+      console.log("Message Sent: " + info.response);
+      res.redirect("/");
+    }
+  });
+});
 app.listen(3000);
 console.log("Server is running on port 3000...");
